f0c706b3597338aba384ef909ec3db4ae70503ce
luguofei <luguofei123@126.com>
2021-01-18 15:14:31 +0800

    'test'

--- src/router/document.js
+++ src/router/document.js
@@ -5,13 +5,13 @@ export const documentRoutes = [
     meta: { moduleName: '公共组件', subModuleName: '公共组件', menuName: '组件使用手册' },
     component: () => import('@/views/document/index.vue'),
     children: [
-      {
-        path: 'buttonExp',
-        moduleCode: 'pub',
-        name: 'buttonExp',
-        meta: { moduleName: '公共组件', subModuleName: '公共组件', menuName: '按钮例子' },
-        component: () => import('@/views/document/manual/buttonExp.vue')
-      }
+      // {
+      //   path: 'buttonExp',
+      //   moduleCode: 'pub',
+      //   name: 'buttonExp',
+      //   meta: { moduleName: '公共组件', subModuleName: '公共组件', menuName: '按钮例子' },
+      //   component: () => import('@/views/document/manual/buttonExp.vue')
+      // }
     ]
   }
 ]
