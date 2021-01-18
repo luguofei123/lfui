b4093651b154ff6b74f2e8d854bb05bb523e2ad8
luguofei <luguofei123@126.com>
2021-01-18 15:20:54 +0800

    '测试revert'

--- src/router/document.js
+++ src/router/document.js
@@ -4,14 +4,14 @@ export const documentRoutes = [
     moduleCode: 'pub',
     meta: { moduleName: '公共组件', subModuleName: '公共组件', menuName: '组件使用手册' },
     component: () => import('@/views/document/index.vue'),
-    children: [
-      {
-        path: 'buttonExp',
-        moduleCode: 'pub',
-        name: 'buttonExp',
-        meta: { moduleName: '公共组件', subModuleName: '公共组件', menuName: '按钮例子' },
-        component: () => import('@/views/document/manual/buttonExp.vue')
-      }
-    ]
+    // children: [
+    //   {
+    //     path: 'buttonExp',
+    //     moduleCode: 'pub',
+    //     name: 'buttonExp',
+    //     meta: { moduleName: '公共组件', subModuleName: '公共组件', menuName: '按钮例子' },
+    //     component: () => import('@/views/document/manual/buttonExp.vue')
+    //   }
+    // ]
   }
 ]
