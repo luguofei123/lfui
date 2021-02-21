ac6421240b9a3a2e15d9bf37f3803dc6a0a4a48b
luguofei <luguofei123@126.com>
2021-02-21 18:11:15 +0800

    'button组件'

--- src/components/common/button/botton.vue
+++ src/components/common/button/botton.vue
@@ -1,5 +1,5 @@
 <template>
-  <button type="button" :disabled="disabled" :class="['lf-btn', size, 'lf-btn-'+type]">
+  <button type="button" :disabled="disabled" :class="['lf-btn', 'lf-btn-size-'+size, 'lf-btn-type-'+type, disabled ? 'lf-btn-disabled' : '']" @click="onClick">
     <span  class="text"><slot></slot></span>
   </button>
 </template>
@@ -19,6 +19,17 @@ export default {
     size: {
       type: String,
       default: 'default'
+      // default small large
+    }
+  },
+  data () {
+    return {
+      tete: ''
+    }
+  },
+  methods: {
+    onClick (e) {
+      this.$emit('click', e)
     }
   }
 }
@@ -27,7 +38,7 @@ export default {
   .lf-btn, .lf-btn:active, .lf-btn:focus{
     outline: 0;
   }
-  .lf-btn-default{
+  .lf-btn-type-default{
     color: #000;
     background-color: #fff;
     border: 1px solid #d9d9d9;
@@ -36,7 +47,18 @@ export default {
       border: 1px solid #4049ff;
     }
   }
-  .lf-btn-primary{
+  .lf-btn-type-default.lf-btn-disabled {
+    color: #c0c4cc;
+    background-color: #fff;
+    border: 1px solid #ebeef5;
+    cursor: not-allowed;
+    &:hover{
+      color: #c0c4cc;
+      background-color: #fff;
+      border: 1px solid #ebeef5;
+    }
+  }
+  .lf-btn-type-primary{
     color: #fff;
     background-color: #096dd9;
     border: 1px solid #096dd9;
@@ -44,6 +66,35 @@ export default {
       background-color: #4049ff;
       border: 1px solid #4049ff;
     }
+  }
+  .lf-btn-type-primary.lf-btn-disabled {
+    color: #fff;
+    background-color: #a0cfff;
+    border: 1px solid #a0cfff;
+    cursor: not-allowed;
+    &:hover{
+      color: #fff;
+      background-color: #a0cfff;
+      border: 1px solid #a0cfff;
+    }
+  }
+  .lf-btn-size-large{
+    height: 32px;
+    padding: 0 15px;
+    font-size: 14px;
+    line-height: 32px;
+  }
+  .lf-btn-size-default{
+    height: 28px;
+    padding: 0 12px;
+    font-size: 13px;
+    line-height: 28px;
+  }
+  .lf-btn-size-small{
+    height: 24px;
+    padding: 0 10px;
+    font-size: 12px;
+    line-height: 24px;
   }
   .lf-btn {
     box-shadow: 0 2px 0 rgb(0 0 0 / 2%);
@@ -54,9 +105,6 @@ export default {
     white-space: nowrap;
     text-align: center;
     touch-action: manipulation;
-    height: 32px;
-    padding: 0 15px;
-    font-size: 14px;
     border-radius: 4px;
   }
 </style>
--- src/views/document/manual/buttonExp.vue
+++ src/views/document/manual/buttonExp.vue
@@ -2,6 +2,30 @@
 <div>
     <lf-button>默认按钮</lf-button>
     &nbsp;&nbsp;
-    <lf-button type="primary">primary</lf-button>
+    <lf-button type="primary" size="small">primary</lf-button>
+    &nbsp;&nbsp;
+    <lf-button type="primary" >primary</lf-button>
+    &nbsp;&nbsp;
+    <lf-button @click="test1" disabled>默认按钮</lf-button>
+    &nbsp;&nbsp;
+    <lf-button type="primary" size="large" @click="test" disabled>primary</lf-button>
 </div>
 </template>
+<script>
+export default {
+  data () {
+    return {
+    }
+  },
+  methods: {
+    test (e) {
+      console.log('test')
+      console.log(e)
+    },
+    test1 (e) {
+      console.log('test1')
+      console.log(e)
+    }
+  }
+}
+</script>
